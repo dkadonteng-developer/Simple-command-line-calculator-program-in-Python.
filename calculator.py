3597ddcbdc4e93bc45dbff6379d9f63f214bb268
Derrick Kwaku Adonteng <138382322+dkadonteng-developer@users.noreply.github.com>
2023-12-15 04:43:37 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,24 +1,18 @@
-# Define a function to add two numbers
 def add(x, y):
     return x + y
 
-# Define a function to subtract two numbers
 def subtract(x, y):
     return x - y
 
-# Define a function to multiply two numbers
 def multiply(x, y):
     return x * y
 
-# Define a function to divide two numbers
 def divide(x, y):
     if y == 0:
         return "Cannot divide by zero"
     return x / y
 
-# Main calculator loop
 while True:
-    # Display the menu
     print("Options:")
     print("Enter 'add' for addition")
     print("Enter 'subtract' for subtraction")
@@ -26,20 +20,15 @@ while True:
     print("Enter 'divide' for division")
     print("Enter 'quit' to end the program")
 
-    # Get the user's choice
     user_input = input(": ")
 
-    # Check if the user wants to quit
     if user_input == "quit":
         break
 
-    # Check if the user's input is a valid operation
     if user_input in ("add", "subtract", "multiply", "divide"):
-        # Get the user's numbers
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        # Perform the selected operation
         if user_input == "add":
             print("Result: ", add(num1, num2))
         elif user_input == "subtract":
